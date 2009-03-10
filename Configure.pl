5aafe1736be8edd0d6c230fb4c95b7c37d3fcc71
Allison Randal <allison@parrot.org>
2009-03-10 20:43:51 +0000

    [pheme] Giving Pheme a Configure.pl that runs on an installed Parrot.

--- Configure.pl
+++ Configure.pl
@@ -1,9 +1,43 @@
 #! perl
-# $Id: Configure.pl 36833 2009-02-17 20:09:26Z allison $
-# Copyright (C) 2009, Parrot Foundation.
 
 use strict;
 use warnings;
 
-chdir '../..';
-`$^X -Ilib tools/dev/reconfigure.pl --step=gen::languages --languages=pheme`;
+my $perlbin = `parrot_config perl`;
+my $libdir = `parrot_config libdir`;
+my $versiondir = `parrot_config versiondir`;
+my $slash = `parrot_config slash`;
+my $make = `parrot_config make`;
+
+chomp($perlbin);
+chomp($libdir);
+chomp($versiondir);
+chomp($slash);
+chomp($make);
+
+my $build_tool = $perlbin . " "
+               . $libdir
+               . $versiondir
+               . $slash
+               . "tools"
+               . $slash
+               . "dev"
+               . $slash
+               . "gen_makefile.pl";
+
+my %makefiles = (
+    "config/makefiles/root.in" => "Makefile",
+);
+
+foreach my $template (keys %makefiles) {
+    my $makefile = $makefiles{$template};
+    print "Creating $makefile\n";
+    system("$build_tool $template $makefile");
+}
+
+print <<"END";
+
+You can now use '$make' to build Pheme.
+END
+
+exit;
